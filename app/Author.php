7e381f16876af8cf92a62a33d164ef4977159e13
Andrius Pilvelis <andrius.pilv@gmail.com>
2020-09-15 12:27:14 +0300

    created fillables

--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    public $fillable = ['name', 'surname'];
+
+    public function book()
+    {
+        return $this->hasMany('App\Book');
+    }
+
 }
--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    public $fillable = ['title', 'pages', 'isbn', 'description', 'author_id'];
+
+    public function author()
+    {
+        return $this->belongsTo('App\Author');
+    }
 }
